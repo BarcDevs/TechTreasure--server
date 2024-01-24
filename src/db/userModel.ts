fad2ee98402fe1cb293235bea74f7257d75d4412
Bar Cohen <sscohen99@gmail.com>
2024-01-24 04:35:00 +0200

    added compare passwords method

--- src/db/userModel.ts
+++ src/db/userModel.ts
@@ -63,6 +63,10 @@ userModel.pre('save', async function (next) {
   next()
 })
 
+userModel.methods.comparePasswords = async function (password: string) {
+  return await bcrypt.compare(password, this.password)
+}
+
 const User = model<IUser>('User', userModel)
 export default User
 
