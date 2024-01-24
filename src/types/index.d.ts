c42d8b9d4ab80990a9bf2d0ecc09fba28cc1ddc9
Bar Cohen <sscohen99@gmail.com>
2024-01-24 04:20:32 +0200

    added typing

---
+++ src/types/index.d.ts
@@ -0,0 +1,87 @@
+import { ObjectId } from 'mongoose'
+
+export type BasicProduct = {
+  id: ObjectId
+  name: string
+  description: string
+  mainImage: string
+  images?: string[],
+  sizes?: string[]
+  rating: number
+  votes: number
+  price: number
+  sale?: number
+  saleEndsAt?: Date
+  oldPrice?: number
+  isNew?: boolean
+  category: string
+  shippingFee?: number
+  stock: number
+}
+
+export type ProductWithColors = Omit<BasicProduct, 'mainImage' | 'images'> & {
+  mainImage: { [key: ProductWithColors['colors'][number]['name']]: string }
+  images?: { [key: ProductWithColors['colors'][number]['name']]: string[] }
+  colors: Color[]
+  defaultColor: ProductWithColors['colors'][number]['name']
+}
+
+export type Product = BasicProduct | ProductWithColors
+
+export type Color = { name: string, hex: string }
+
+export type CartItem = Product & {
+  itemVariants?: [{ color?: string, size?: string }]
+  quantity: number
+  subtotal: number
+}
+
+export type ManagedProduct = Product & {
+  orders: Order[] | string[]
+}
+
+export type Order = {
+  id: string
+  createdAt: Date
+  items: CartItem[]
+  payment: string
+  status: 'pending' | 'processing' | 'shipped' | 'delivered'
+  total: number
+  trackingNumber?: string
+}
+
+export type User = {
+  name: string
+  email: string
+  role: 'user' | 'seller'
+  billingDetails?: BillingDetails[]
+  cart?: Cart
+  orders?: Order[]
+  createdAt: Date
+  resetToken?: string
+  resetTokenExpiration?: Date
+}
+
+export type Cart = {
+  items: CartItem[]
+  totalItems: number
+  subtotal: number
+  discount?: {
+    percent?: number
+    fixed?: number
+  }
+  cartDiscount?: number
+  shipping?: number
+  total: number
+}
+
+export type Category = {
+  id: string
+  name: string
+  icon: string
+  items?: Product[] // populated by mongoose
+}
+
+export type BillingDetails = {
+
+}
