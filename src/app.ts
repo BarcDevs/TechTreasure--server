a52c0ccfd8134d5ae113c94a26943220b4025a22
Bar Cohen <sscohen99@gmail.com>
2024-01-26 04:42:01 +0200

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express'
+import cors from 'cors'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import 'dotenv/config'
 
 import { authRouter, storeRouter } from './routes'
 import { errorHandler, notFound } from './controllers/errorController'
 
 const app = express()
+
+/* cors */
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin', 'X-Auth-Token', 'X-Request-With']
+}))
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -14,7 +23,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/api/items', storeRouter)
+app.use('/api/products', storeRouter)
 app.use('/api/auth', authRouter)
 
 app.all('*', notFound)
