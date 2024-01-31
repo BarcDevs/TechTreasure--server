890bd3cda1df609d265edbbe34080cad1341f256
Bar Cohen <sscohen99@gmail.com>
2024-01-31 02:15:33 +0200

    fixed static folder path

--- src/app.ts
+++ src/app.ts
@@ -19,9 +19,9 @@ app.use(cors({
 
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use('/api/products', storeRouter)
 app.use('/api/auth', authRouter)
