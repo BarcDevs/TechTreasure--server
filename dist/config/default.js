1c8cfef9a7aa8f49efd4fda490a729446f5a558b
Bar Cohen <sscohen99@gmail.com>
2024-02-28 02:31:20 +0200

    [build] REMOVED BUILD FILES

--- dist/config/default.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: process.env.PORT || 3000,
-    protocol: process.env.PROTOCOL || 'http',
-    host: process.env.HOST || 'localhost',
-    origin: process.env.ORIGIN || 'http://localhost:5173',
-    url: process.env.URL || 'http://localhost:3000',
-};
