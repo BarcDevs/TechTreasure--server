150d673850a9fc25ecaee501fcdde19480d451fb
Bar Cohen <sscohen99@gmail.com>
2024-02-28 02:31:20 +0200

    [build] ADDED NEW BUILD FILES (v1.1.1)

--- dist/config/default.js
+++ dist/config/default.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
+    env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     protocol: process.env.PROTOCOL || 'http',
     host: process.env.HOST || 'localhost',
