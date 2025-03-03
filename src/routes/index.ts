3b59df9e6d82212249d25caa0d4838b0d56095f4
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-03-03 12:33:50 +0200

    feat: add stripe router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
+import express from 'express'
 import storeRouter from './storeRouter'
 import authRouter from './authRouter'
 import userRouter from './userRouter'
-import express from 'express'
+import paymentRouter from './paymentRouter'
 
 const router = express.Router()
 
 router.use('/products', storeRouter)
 router.use('/auth', authRouter)
 router.use('/user', userRouter)
+router.use('/payment', paymentRouter)
 
 export default router
