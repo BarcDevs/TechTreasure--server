09d01e5cd9b7bbd5fbbc7f1831c393cff9907361
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-03-05 19:30:53 +0200

    feat: add support for search and category

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -3,8 +3,33 @@ import { PopulateOptions } from 'mongoose'
 import { UrlQuery } from '../types'
 import { queryFactory } from './queryFactory'
 
-export const getItemsByQuery = async (urlQuery: UrlQuery, find: object, populateOptions?: PopulateOptions) =>
-  queryFactory<typeof Item>(Item, urlQuery, find, populateOptions)
+export const getItemsByQuery = async (urlQuery: UrlQuery, find: object, populateOptions?: PopulateOptions) => {
+  const pageLimit = Math.max(Number(urlQuery.limit) || 20, 1)
+  const totalPages = Math.ceil(await Item.countDocuments() / pageLimit)
+
+  if (urlQuery.category)
+    find = { ...find, category: urlQuery.category }
+
+  if (urlQuery.search) {
+    const searchTerm = urlQuery.search
+    find = {
+      ...find,
+      $or: [
+        { name: { $regex: searchTerm, $options: 'i' } },
+        { description: { $regex: searchTerm, $options: 'i' } }
+      ]
+    }
+  }
+
+  const items = await queryFactory<typeof Item>(
+    Item, urlQuery, find, populateOptions
+  )
+
+  return {
+    totalPages,
+    products: items
+  }
+}
 
 export const getItemById = async (id: string) => Item.findById(id)
 
